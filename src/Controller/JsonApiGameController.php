0aed43d2d31665776de060c769f5f6777ca2bb4d
Hafsa Choudhury <hafsa05mat@gmail.com>
2024-05-21 13:54:21 +0200

    Added more class by dividing the existing classes

---
+++ src/Controller/JsonApiGameController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+use App\Game21\Game21;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class JsonApiGameController extends AbstractController
+{
+    #[Route("/api/game", name: "api_game21", methods: ['POST'])]
+    public function apiGame21(
+        SessionInterface $session
+    ): Response {
+        $data = [
+            "currentPlayer" => "",
+            "playerCards" => [],
+            "playerCardTotal" => 0,
+            "bankCards" => [],
+            "bankCardTotal" => 0,
+            "winner" => "",
+            "loser" => ""
+        ];
+
+        $game = null;
+
+        if ($session->has("game")) {
+            $game = $session->get("game");
+        }
+
+        if ($game instanceof Game21) {
+            $currentPlayer = $game->getCurrentPlayerInQueue();
+            $data["currentPlayer"] = $currentPlayer->name;
+            $winStatus = $game->checkWinStatus();
+
+            if ($winStatus["loser"]) {
+                $data["winner"] = $winStatus["winner"];
+                $data["loser"] = $winStatus["loser"];
+            }
+        }
+
+        if ($session->has("player_cards")) {
+            $data["playerCards"] = $session->get("player_cards");
+        }
+
+        if ($session->has("player_card_total")) {
+            $data["playerCardTotal"] = $session->get("player_card_total");
+        }
+
+        if ($session->has("bank_cards")) {
+            $data["bankCards"] = $session->get("bank_cards");
+        }
+
+        if ($session->has("bank_card_total")) {
+            $data["bankCardTotal"] = $session->get("bank_card_total");
+        }
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}
